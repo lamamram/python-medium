a91c683f815406bb2a721f3800a80e7f4b2ab3d0
mlamamra <mlamamra@dawan.fr>
2022-07-05 10:13:41 +0200

    exo md5 corrigé

--- exo_md5.py
+++ exo_md5.py
@@ -1,9 +1,10 @@
 # %%
 from hashlib import md5
+from itertools import product, count
 from string import ascii_lowercase
 
-h = md5(bytes("coucou", encoding="utf8"))
-print(h.hexdigest())
+# h = md5(bytes("coucou", encoding="utf8"))
+# print(h.hexdigest())
 
 target= "3ed7dceaf266cafef032b9d5db224717"
 
@@ -11,6 +12,20 @@ target= "3ed7dceaf266cafef032b9d5db224717"
 # HINT: faire une attaque brute force
 # => toute les chaines possibles
 # à partir des lettre minuscules et pas d'espace
+for i in count(1):
+    for p in product(ascii_lowercase, repeat=i):
+        s = "".join(p)
+        if md5(bytes(s, encoding="utf8")).hexdigest() == target:
+            print(s)
+            break
+    else: continue
+    break
 
 
 
+# %%
+
+print(list(product("abcd", repeat=3)))
+
+
+# %%
