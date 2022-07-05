b4e7ecfb01a528b0634a2cb4e570607dcd810329
mlamamra <mlamamra@dawan.fr>
2022-07-05 10:48:22 +0200

    exo groupby

--- exo_codons.py
+++ exo_codons.py
@@ -1,15 +1,22 @@
 # %%
 # découper une chaine de caractère en triplets
 from random import randint, shuffle
+from itertools import islice
 
 # fabrication d'une chaine d'adn random
-sequence = "".join([ (3*randint(1, 10)) * char for char in "ATCG" ])
+sequence = "".join([ (3*randint(1, 4)) * char for char in "ATCG" ])
 sequence = list(sequence)
 shuffle(sequence)
 sequence = "".join(sequence)
 sequence
 
+# %%
 # transformer la chaine en liste de chaines de longueur 3
+l = len(sequence)
+slices = [ islice(sequence, i, l, 3) for i in range(3) ]
+tuples = list(zip(*slices))
+list(map(lambda t: "".join(t) , tuples))
 
-
+# %%
+[ sequence[i:i+3] for i in range(0, l, 3) ]
 # %%
